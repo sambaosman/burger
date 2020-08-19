0b53c1bfdd6f5f3fffaf21bdfa9e85daff361b3c
sambaosman <contactsalmaosman@gmail.com>
2020-08-19 15:32:29 -0400

    original transformedIngredients array to for loop

--- src/Burger/Burger.js
+++ src/Burger/Burger.js
@@ -3,46 +3,49 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-  console.log("Test");
-  //   let newArray = [];
-  //   for (let [first, number] of Object.entries(props.ingredients)) {
-  //     console.log(first);
-  //     let Array = [];
-  //     for (let i = 0; i < number; i++) {
-  //       Array.push(""); //for each number, it makes an array of the inventory of the item
-  //     }
-  //     console.log(Array); //["",""]
-  //     newArray.push(Array);
-  //   }
-  //   console.log(newArray); //[["",""],[""],["",""]]
-  //you want to transform a string into an array with as many elements as we have ingredients for a given ingredient
-  //if we have two cheese ingredients, then he wants to transform the cheese string into an array that has 'x' elements. array that should have a length of two.
-  let transformedIngredients = Object.keys(
-    //keys returns an array of the keys
-    //turns state from BurgerBuilder object into an array
-    props.ingredients //takes in prop.ingredients
-  )
-    .map((igKey) => {
-      //map goes through each element in the array
-      console.log(igKey); //each individual type ex. salad, meat, bacon etc.
-      //spread operator to make a new array
-      return (
-        [...Array(props.ingredients[igKey])] //Array(3) would make an array of 3 empty spaces so we want to access the number inside ingredients that's associated with a specific key
-          // input: 'cheese'
-          // output: ['', '']
-          .map((_, i) => {
-            //goes through both items in the small array helps focus only on the amount (index)
-            //igKey is the type like "salad or meat"
-            return <BurgerIngredient key={igKey + i} type={igKey} />;
-          })
-      );
-      // [[<BurgerIngredient key={cheese + 0} type={cheese} />, <BurgerIngredient key={cheese + 1} type={cheese} />], ]
-    })
-    .reduce((prevValue, currValue) => {
-      //reduces the array to a single value
-      //takes in a previous value, current value and an array
-      return prevValue.concat(currValue);
-    }, []);
+  let transformedIngredients = [];
+  for (let [item, number] of Object.entries(props.ingredients)) {
+    console.log(item);
+    let Array = [];
+    for (let i = 0; i < number; i++) {
+      Array.push(
+        <BurgerIngredient key={item + i} type={item}></BurgerIngredient>
+      ); //for each number, it makes an array of the inventory of the item
+    }
+    console.log(Array); //["",""]
+    transformedIngredients.push(Array);
+    console.log(transformedIngredients); //[["",""],[""],["",""]]
+  }
+
+  // //you want to transform a string into an array with as many elements as we have ingredients for a given ingredient
+  // //if we have two cheese ingredients, then he wants to transform the cheese string into an array that has 'x' elements. array that should have a length of two.
+  // let transformedIngredients = Object.keys(
+  //   //keys returns an array of the keys
+  //   //turns state from BurgerBuilder object into an array
+  //   props.ingredients //takes in prop.ingredients
+  // )
+  //   .map((igKey) => {
+  //     //map goes through each element in the array
+  //     console.log(igKey); //each individual type ex. salad, meat, bacon etc.
+  //     //spread operator to make a new array
+  //     return (
+  //       [...Array(props.ingredients[igKey])] //Array(3) would make an array of 3 empty spaces so we want to access the number inside ingredients that's associated with a specific key
+  //         // input: 'cheese'
+  //         // output: ['', '']
+  //         .map((_, i) => {
+  //           //goes through both items in the small array helps focus only on the amount (index)
+  //           //igKey is the type like "salad or meat"
+  //           return <BurgerIngredient key={igKey + i} type={igKey} />;
+  //         })
+  //     );
+  //     // [[<BurgerIngredient key={cheese + 0} type={cheese} />, <BurgerIngredient key={cheese + 1} type={cheese} />], ]
+  //   })
+  transformedIngredients.reduce((newArray, currValue) => {
+    //currValue is each empty array
+    //reduces the array to a single value
+    //takes in a previous value, current value and an array
+    return newArray.concat(currValue);
+  }, []);
   if (transformedIngredients.length === 0) {
     transformedIngredients = <p>Please start adding ingredients</p>;
   }
@@ -56,5 +59,4 @@ const burger = (props) => {
     </div>
   );
 };
-
 export default burger;
